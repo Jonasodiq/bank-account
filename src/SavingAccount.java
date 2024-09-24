ba78557462c22f9e4c179f99b3d09618a1c3009d
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-09-24 11:16:39 +0200

    Comment

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,15 +1,19 @@
 public class SavingAccount extends Account {
     private double interestRate;
+
+    // Konstruktor som tar emot en räntesats
     public SavingAccount(String name, double balance, double interestRate) {
-        super(name, balance);
+        super(name, balance); // Anropa superklassens konstruktor
         this.interestRate = interestRate;
     }
 
+    // Metod för att lägga till ränta till balansen
     public void addInterest() {
         double interest = getBalance() * ( interestRate / 100);
         deposit(interest);
     }
 
+    // Get & Setters
     public double getInterestRate() {
         return interestRate;
     }
